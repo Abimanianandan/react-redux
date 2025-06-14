f12469a53b78adeeaa0a543c525faf09a782217d
Abimani18 <Abimani@imaggar.in>
2025-06-14 17:00:56 +0530

    add loading gif

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import DisplayUsers from './components/DisplayUsers'
 import AddUser from './components/AddUser'
 import EditUser from './components/EditUser'
+import LoadingImage from '../src/preloader.gif'
 
 const App = () => {
+  const [loading,setLoading] = useState(true)
+ useEffect(()=>{
+   const timer = setTimeout(()=>{
+    setLoading(false)
+    return ()=> clearTimeout(timer)
+   },1000)
+ },[])
   return (
-    <BrowserRouter>
+   <>
+   {loading ? <div className='d-flex justify-content-center' style={{marginTop:"100px"}}>
+      <img src={LoadingImage} alt="loading..." />
+   </div> : <BrowserRouter>
       <Routes>
-        <Route path='/' element={<DisplayUsers />}/>
-        <Route path='/add' element={<AddUser />}/>
-        <Route path='/:id' element={<EditUser />}/>
+          <Route path='/' element={<DisplayUsers />}/>
+          <Route path='/add' element={<AddUser />}/>
+          <Route path='/:id' element={<EditUser />}/>
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter>}
+   </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux'
 import store from './components/Store.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
   <Provider store={store}>
